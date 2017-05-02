01316cab6ae406321d1a3bc98345b5187c3e3465
h4nk-et0 <hank.eto.echo@gmail.com>
2017-05-02 13:14:21 +0530

    added windows support

--- birthday alert/src/alert.py
+++ birthday alert/src/alert.py
@@ -10,6 +10,45 @@
 import time
 from utils.utils import *
 
+def help() :
+	print \
+"""Usage: alert.py [options]
+
+OPTIONS :
+
+	-h or --help\t\t\t- show this help
+	-a or -s or --add or --show\t- add a new event to the list or show the event dialog
+"""
+
+if len(sys.argv) == 2 :
+	if sys.argv[1] in ['-h', '--help'] :
+		help()
+		quit()
+
+	elif sys.argv[1] in ['-a', '-s', '--add', '--show'] :
+		fileData = readFile()
+		events = eventCheck(fileData)
+
+		if events != [] :
+			for event in events :
+				showData = '\nToday is ' + event[0] + "'s birthday." + '\nDate: ' + "%02d" %(event[1][0]) + '-' + "%02d\n" %(event[1][1])
+				eventShow(showData)
+				time.sleep(2)
+		else :
+			eventShow("\nThere are no events today ...\nIf you wish to add a new event click 'add new'\n")
+
+		quit()
+
+	else :
+		print 'invalid argument'
+		help()
+		quit()
+
+elif len(sys.argv) > 2 :
+	help()
+	quit()
+
+
 while True :
 	noEvent = False
 	currentDate = QDate.currentDate()
@@ -18,7 +57,7 @@ while True :
 
 	if events != [] :
 		for event in events :
-			showData = '\nToday is ' + event[0] + "'s birthday." + ' Wish them best!\n' + 'Date: ' + str(event[1][0]) + '-' + str(event[1][1])
+			showData = '\nToday is ' + event[0] + "'s birthday." + '\nDate: ' + "%02d" %(event[1][0]) + '-' + "%02d\n" %(event[1][1])
 			eventShow(showData)
 			time.sleep(2)
 
--- birthday alert/src/utils/graphics.py
+++ birthday alert/src/utils/graphics.py
@@ -5,6 +5,8 @@ from PyQt4.QtGui import *
 import sys
 import os
 
+# main event called from alert.py
+
 def eventShow(showString) :
 	windowframe = windowFrame()
 	windowframe.mainwindow.body.setText(showString)
@@ -19,50 +21,50 @@ class globalData() :
 
 		if sys.platform == "linux" or sys.platform == "linux2" :
 			HOME = os.getenv("HOME")
-			self.DB_FILE_PATH = os.path.join(HOME,'.config/utility_belt/birthday alert/birthday_db')
+			self.DB_FILE_PATH = os.path.join(HOME, '.config/utility_belt/birthday alert/birthday_db')
 
-			if not os.path.exists(os.path.join(HOME,'.config')) :
+			if not os.path.exists(os.path.join(HOME, '.config')) :
 				try :
-					os.mkdir(os.path.join(HOME,'.config/'))
-					os.mkdir(os.path.join(HOME,'.config/utility_belt/'))
-					os.mkdir(os.path.join(HOME,'.config/utility_belt/birthday alert'))
+					os.mkdir(os.path.join(HOME, '.config/'))
+					os.mkdir(os.path.join(HOME, '.config/utility_belt/'))
+					os.mkdir(os.path.join(HOME, '.config/utility_belt/birthday alert'))
 				except :
 					eventShow('error while creating ' + self.DB_FILE_PATH)
 					quit()
 
 				try :
-					birthday_db = open(self.DB_FILE_PATH,'w')
+					birthday_db = open(self.DB_FILE_PATH, 'w')
 
 					birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
 					birthday_db.close()
 				except :
 					eventShow('error while creating ' + self.DB_FILE_PATH)
 
-			elif not os.path.exists(os.path.join(HOME,'.config/utility_belt/')) :
+			elif not os.path.exists(os.path.join(HOME, '.config/utility_belt/')) :
 				try :
-					os.mkdir(os.path.join(HOME,'.config/utility_belt/'))
-					os.mkdir(os.path.join(HOME,'.config/utility_belt/birthday alert'))
+					os.mkdir(os.path.join(HOME, '.config/utility_belt/'))
+					os.mkdir(os.path.join(HOME, '.config/utility_belt/birthday alert'))
 				except :
 					eventShow('error while creating ' + self.DB_FILE_PATH)
 					quit()
 
 				try :
-					birthday_db = open(self.DB_FILE_PATH,'w')
+					birthday_db = open(self.DB_FILE_PATH, 'w')
 
 					birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
 					birthday_db.close()
 				except :
 					eventShow('error while creating ' + self.DB_FILE_PATH)
 
-			elif not os.path.exists(os.path.join(HOME,'.config/utility_belt/birthday alert')) :
+			elif not os.path.exists(os.path.join(HOME, '.config/utility_belt/birthday alert')) :
 				try :
-					os.mkdir(os.path.join(HOME,'.config/utility_belt/birthday alert'))
+					os.mkdir(os.path.join(HOME, '.config/utility_belt/birthday alert'))
 				except :
 					eventShow('error while creating ' + DB_FILE_PATH)
 					quit()
 
 				try :
-					birthday_db = open(self.DB_FILE_PATH,'w')
+					birthday_db = open(self.DB_FILE_PATH, 'w')
 
 					birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
 					birthday_db.close()
@@ -72,7 +74,7 @@ class globalData() :
 			elif not os.path.exists(self.DB_FILE_PATH) :
 
 				try :
-					birthday_db = open(self.DB_FILE_PATH,'w')
+					birthday_db = open(self.DB_FILE_PATH, 'w')
 
 					birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
 					birthday_db.close()
@@ -80,8 +82,8 @@ class globalData() :
 					eventShow('error while creating ' + self.DB_FILE_PATH)
 
 		elif sys.platform == "win32" :
-			print 'windows test'
-			self.DB_FILE_PATH = "C:\ProgramData\UtilityBelt\BirthdayAlert\birthday_db"
+			self.DB_FILE_PATH = "C:\ProgramData\UtilityBelt\BirthdayAlert\\birthday_db"
+			self.DB_FILE_PATH = self.DB_FILE_PATH.replace("\\", "/")
 
 			if not os.path.exists("C:\ProgramData") :
 				try :
@@ -93,7 +95,7 @@ class globalData() :
 					quit()
 
 				try :
-					birthday_db = open(self.DB_FILE_PATH,'w')
+					birthday_db = open(self.DB_FILE_PATH, 'w')
 
 					birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
 					birthday_db.close()
@@ -109,7 +111,7 @@ class globalData() :
 					quit()
 
 				try :
-					birthday_db = open(self.DB_FILE_PATH,'w')
+					birthday_db = open(self.DB_FILE_PATH, 'w')
 
 					birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
 					birthday_db.close()
@@ -124,7 +126,7 @@ class globalData() :
 					quit()
 
 				try :
-					birthday_db = open(self.DB_FILE_PATH,'w')
+					birthday_db = open(self.DB_FILE_PATH, 'w')
 
 					birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
 					birthday_db.close()
@@ -133,54 +135,46 @@ class globalData() :
 
 			elif not os.path.exists(self.DB_FILE_PATH) :
 
-#				try :
-				print self.DB_FILE_PATH
-				birthday_db = open(self.DB_FILE_PATH,'w')
+				try :
+					birthday_db = open(self.DB_FILE_PATH, 'w')
 
-				birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
-				birthday_db.close()
-#				except :
-#					eventShow('error while creating' + self.DB_FILE_PATH)
+					birthday_db.write('# edit this file with caution. wrong format leads to the program crash.\n')
+					birthday_db.close()
+				except :
+					eventShow('error while creating' + self.DB_FILE_PATH)
 			
 # widget for adding new birthdays
 
 class addWindow(QDialog) :
-	def __init__(self,parent = None) :
-		super(addWindow,self).__init__(parent)
+	def __init__(self, parent = None) :
+		super(addWindow, self).__init__(parent)
 
 		style_css = """
 
 		QPushButton {
 			background: #9F440D;
 			font-family: Monospace;
-			font-size: 14px;
+			font-size: 13px;
 			font-weight: bold;
 			color: #000000;
-			outline:0;
+			outline: 0;
 			border-radius: 6px;
-			padding: 10px;
+			padding: 5px;
+			height: 15px;
+			width: 60px;
 		}
 
-		QPushButton:pressed {
+		QPushButton:pressed, QPushButton:hover, QPushButton:selected {
 			background: #603108;
 			font-family: Monospace;
-			font-size: 14px;
+			font-size: 15px;
 			font-weight: bold;
 			color: #000000;
 			outline:0;
 			border-radius: 0px;
-			padding: 10px;
-		}
-
-		QPushButton:hover {
-			background: #603108;
-			font-family: Monospace;
-			font-size: 14px;
-			font-weight: bold;
-			color: #000000;
-			outline:0;
-			border-radius: 0px;
-			padding: 10px;
+			padding: 0px;
+			height: 15px;
+			width: 60px;
 		} """
 
 		self.setWindowTitle('add new alert')
@@ -189,10 +183,10 @@ class addWindow(QDialog) :
 		self.nameBox.selectAll()
 
 		self.monthBox = QComboBox()
-		self.monthBox.insertItems(0,map(str,range(1,13)))
+		self.monthBox.insertItems(0, map(str, range(1, 13)))
 
 		self.dateBox = QComboBox()
-		self.dateBox.insertItems(0,map(str,range(1,32)))
+		self.dateBox.insertItems(0, map(str, range(1, 32)))
 
 		self.namelabel = QLabel('name :')
 		self.namelabel.setBuddy(self.nameBox)
@@ -210,33 +204,36 @@ class addWindow(QDialog) :
 		self.buttonlayout.addStretch(1)
 		self.buttonlayout.addWidget(self.add)
 		self.buttonlayout.addWidget(self.cancel)
-		self.layout = QGridLayout(self)
-		self.layout.addWidget(self.namelabel,0,0)
-		self.layout.addWidget(self.nameBox,0,1,1,3)
-		self.layout.addWidget(self.monthlabel,1,0)
-		self.layout.addWidget(self.monthBox,1,1)
-		self.layout.addWidget(self.datelabel,1,2)
-		self.layout.addWidget(self.dateBox,1,3)
-		self.layout.addLayout(self.buttonlayout,3,0,1,4)
+		self.buttonlayout.setMargin(20)
+		self.buttonlayout.setSpacing(8)
 
-		self.connect(self.monthBox,SIGNAL("currentIndexChanged(int)"),self.setDateBox)
+		self.layout = QGridLayout(self)
+		self.layout.addWidget(self.namelabel, 0, 0)
+		self.layout.addWidget(self.nameBox, 0, 1, 1, 3)
+		self.layout.addWidget(self.monthlabel, 1, 0)
+		self.layout.addWidget(self.monthBox, 1, 1)
+		self.layout.addWidget(self.datelabel, 1, 2)
+		self.layout.addWidget(self.dateBox, 1, 3)
+		self.layout.addLayout(self.buttonlayout, 3, 0, 1, 4)
+
+		self.connect(self.monthBox, SIGNAL("currentIndexChanged(int)"), self.setDateBox)
 
 	def setDateBox(self) :
 		month = self.monthBox.currentIndex() + 1
 
 		if month == 2 :
 			self.dateBox.clear()
-			self.dateBox.insertItems(0,map(str,range(1,30)))
+			self.dateBox.insertItems(0, map(str, range(1, 30)))
 
-		elif month not in [1,3,5,7,8,10,12] :
+		elif month not in [1, 3, 5, 7, 8, 10, 12] :
 			self.dateBox.clear()
-			self.dateBox.insertItems(0,map(str,range(1,31)))
+			self.dateBox.insertItems(0, map(str, range(1, 31)))
 
 # widget for showing all birthdays
 
 class allBdays(QDialog) :
-	def __init__(self,parent = None) :
-		super(allBdays,self).__init__(parent)
+	def __init__(self, parent = None) :
+		super(allBdays, self).__init__(parent)
 
 		style_css = """
 
@@ -255,8 +252,8 @@ class allBdays(QDialog) :
 # widget for windows title
 
 class windowTitle(QDialog) :
-	def __init__(self,parent = None) :
-		super(windowTitle,self).__init__(parent)
+	def __init__(self, parent = None) :
+		super(windowTitle, self).__init__(parent)
 
 		style_css = """
 		QLabel {
@@ -279,14 +276,14 @@ class windowTitle(QDialog) :
 		self.layout.setSpacing(0)
 		self.layout.setMargin(3)
 
-		self.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Fixed)
+		self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 		self.setStyleSheet(style_css)
 
 # side frame widget
 
 class sideFrame(QDialog) :
 	def __init__(self, parent = None) :
-		super(sideFrame,self).__init__(parent)
+		super(sideFrame, self).__init__(parent)
 
 		style_css = """
 		QDialog {
@@ -302,14 +299,16 @@ class sideFrame(QDialog) :
 
 class mainWindow(QDialog) :
 	def __init__(self, parent = None) :
-		super(mainWindow,self).__init__(parent)
+		super(mainWindow, self).__init__(parent)
 
 		try :
 			image_path = sys._MEIPASS
-			image_path = os.path.join(image_path,'background.png')
+			image_path = os.path.join(image_path, 'background.png')
 		except :
 			image_path = os.path.realpath(__file__)
 			image_path = image_path[:image_path.find("graphics.py")] + "background.png"
+			if sys.platform == "win32" :
+				image_path = image_path.replace('\\', "/")
 
 		style_css = """
 
@@ -331,39 +330,47 @@ class mainWindow(QDialog) :
 		QPushButton {
 			background: #9F440D;
 			font-family: Monospace;
-			font-size: 14px;
+			font-size: 13px;
 			font-weight: bold;
 			color: #000000;
-			outline:0;
+			outline: 0;
 			border-radius: 6px;
-			padding: 10px;
+			padding-top: 10px;
+			padding-bottom: 10px;
+			padding-left: 5px;
+			padding-right: 5px;
+			height: 15px;
+			width: 60px;
 		}
 
-		QPushButton:pressed {
+		QPushButton:pressed, QPushButton:hover, QPushButton:selected {
 			background: #603108;
 			font-family: Monospace;
-			font-size: 14px;
+			font-size: 15px;
 			font-weight: bold;
-			color: #000000;
+			color: grey;
 			outline:0;
 			border-radius: 0px;
-			padding: 10px;
-		}
-
-		QPushButton:hover {
-			background: #603108;
-			font-family: Monospace;
-			font-size: 14px;
-			font-weight: bold;
-			color: #000000;
-			outline:0;
-			border-radius: 0px;
-			padding: 10px;
+			padding: 0px;
+			height: 15px;
+			width: 60px;
 		}
 
 		QDialog {
 			background: #0BF4AB;
 			background-image: url(""" + image_path + """);
+		}
+
+		QCheckBox {
+			color: #F79E05;
+			font-family: Monospace;
+			font-weight: bold;
+			background: #302210;
+		}
+
+		QCheckBox:hover {
+			color: none;
+			background: none;
 		} """
 
 		self.body = QLabel()
@@ -376,6 +383,7 @@ class mainWindow(QDialog) :
 
 		self.gotit = QPushButton('&Got It')
 		self.gotit.setStyleSheet(style_css)
+		self.gotit.setShortcut("space")
 
 		self.add_new = QPushButton('&Add new')
 		self.add_new.setStyleSheet(style_css)
@@ -394,7 +402,7 @@ class mainWindow(QDialog) :
 		self.checkBox_layout = QHBoxLayout()
 		self.checkBox_layout.addStretch(1)
 		self.checkBox_layout.addWidget(self.checkAgain)
-		self.checkBox_layout.setMargin(0)
+		self.checkBox_layout.setMargin(12)
 		self.checkBox_layout.setSpacing(0)
 
 		self.button_layout = QHBoxLayout()
@@ -403,7 +411,7 @@ class mainWindow(QDialog) :
 		self.button_layout.addWidget(self.add_new)
 		self.button_layout.addWidget(self.gotit)
 		self.button_layout.setMargin(0)
-		self.button_layout.setSpacing(5)
+		self.button_layout.setSpacing(8)
 
 		self.layout = QVBoxLayout()
 		self.layout.addLayout(self.body_layout)
@@ -419,7 +427,7 @@ class mainWindow(QDialog) :
 
 class windowFrame(QFrame) :
 	def __init__(self, parent = None) :
-		super(windowFrame,self).__init__(parent)
+		super(windowFrame, self).__init__(parent)
 
 		self.setWindowFlags(Qt.FramelessWindowHint)
 		self.setFrameShadow(QFrame.Sunken)
@@ -441,13 +449,13 @@ class windowFrame(QFrame) :
 		self.layout.setSpacing(0)
 
 		self.setLayout(self.layout)
-		self.resize(QDesktopWidget().screenGeometry().width(),180)
-		self.move(0,(QDesktopWidget().screenGeometry().height() / 2) - 90)
+		self.resize(QDesktopWidget().screenGeometry().width(), 180)
+		self.move(0, (QDesktopWidget().screenGeometry().height() / 2) - 90)
 
-		self.connect(self.mainwindow.gotit,SIGNAL('clicked()'),app.exit)
-		self.connect(self.mainwindow.add_new,SIGNAL('clicked()'),self.addNew)
-		self.connect(self.mainwindow.show_all,SIGNAL('clicked()'),self.showAll)
-		self.connect(self.mainwindow.checkAgain,SIGNAL('toggled(bool)'),self.updateShow)
+		self.connect(self.mainwindow.gotit, SIGNAL('clicked()'), app.exit)
+		self.connect(self.mainwindow.add_new, SIGNAL('clicked()'), self.addNew)
+		self.connect(self.mainwindow.show_all, SIGNAL('clicked()'), self.showAll)
+		self.connect(self.mainwindow.checkAgain, SIGNAL('toggled(bool)'), self.updateShow)
 
 	def updateShow(self) :
 		if self.mainwindow.checkAgain.isChecked() :
@@ -460,18 +468,20 @@ class windowFrame(QFrame) :
 		self.allbdays.show()
 
 		try :
-			db_file = open(globaldata.DB_FILE_PATH,'r+')
-			db_file.seek(0,0)
+			db_file = open(globaldata.DB_FILE_PATH, 'r+')
+			db_file.seek(0, 0)
 		except :
-			self.allbdays.showText.append('cannot open ' + globaldata.DB_FILE_PATH)
-			quit()
+			self.allbdays.hide()
+			self.mainwindow.body.setText('cannot open ' + globaldata.DB_FILE_PATH)
+			return
 
 		try :
 			file_data = db_file.readlines()
 			db_file.close()
 		except :
-			self.allbdays.showText.append('cannot read from ' + globaldata.DB_FILE_PATH)
-			quit()
+			self.allbdays.hide()
+			self.mainwindow.body.setText('cannot read from ' + globaldata.DB_FILE_PATH)
+			return
 
 		for data in file_data :
 			if data[0] == '#' :
@@ -481,17 +491,21 @@ class windowFrame(QFrame) :
 
 			try :
 				data = data.strip()
-				data = [data[:data.rfind('\t')].strip(),map(int,data[data.rfind('\t'):].strip().split('-'))]
-				self.allbdays.showText.append(data[0] + '\t\t' + str(data[1][0]) + '-' + str(data[1][0]))
+				data = [data[:data.rfind('\t')].strip(), map(int, data[data.rfind('\t'):].strip().split('-'))]
+				if len(data[0]) > 9 :
+					self.allbdays.showText.append("%s\t%02d-%02d"%(data[0], data[1][0], data[1][1]))
+				else :
+					self.allbdays.showText.append("%s\t\t%02d-%02d"%(data[0], data[1][0], data[1][1]))
 			except :
-				self.allbdays.showText.append('invalid table in ' + globaldata.DB_FILE_PATH)
-				quit()
+				self.allbdays.hide()
+				self.mainwindow.body.setText('invalid table in ' + globaldata.DB_FILE_PATH)
+				return
 
 	def addNew(self) :
 		self.addwindow = addWindow(self)
 
-		self.addwindow.connect(self.addwindow.cancel,SIGNAL('clicked()'),self.addwindow.hide)
-		self.addwindow.connect(self.addwindow.add,SIGNAL('clicked()'),self.addNewAdd)
+		self.addwindow.connect(self.addwindow.cancel, SIGNAL('clicked()'), self.addwindow.hide)
+		self.addwindow.connect(self.addwindow.add, SIGNAL('clicked()'), self.addNewAdd)
 		self.addwindow.show()
 
 	def addNewAdd(self) :
@@ -502,24 +516,31 @@ class windowFrame(QFrame) :
 		self.addwindow.hide()
 
 		try :
-			db_file = open(globaldata.DB_FILE_PATH,'r+')
-			db_file.seek(-1,2)
+			db_file = open(globaldata.DB_FILE_PATH, 'r+')
+
+			if sys.platform == "win32" :
+				db_file.seek(0, 2)		# windows gives error on negative offset
+			else :
+				db_file.seek(-1, 2)
+
 			if db_file.read() == '\n' :
 				writeData = str()
 			else :
 				writeData = '\n'
-			
 		except :
-			eventShow('cannot open ' + globaldata.DB_FILE_PATH)
+			self.mainwindow.body.setText('cannot open ' + globaldata.DB_FILE_PATH)
 			quit()
 
 		if len(self.name) >= 8 :
-			writeData += self.name + '\t' + str(self.date) + '-' + str(self.month) + '\n'
+			writeData += self.name + '\t'
 		else :
-			writeData += self.name + '\t\t' + str(self.date) + '-' + str(self.month) + '\n'
+			writeData += self.name + '\t\t'
 
-		db_file.write(writeData)
+		db_file.write("%s%02d-%02d\n" %(writeData, self.date, self.month))
 		db_file.close()
+		text = 	self.mainwindow.body.text()
+		text += "\nname: %s is added to the list" %(self.name)
+		self.mainwindow.body.setText(text)
 
 app = QApplication(sys.argv)
 globaldata = globalData()
